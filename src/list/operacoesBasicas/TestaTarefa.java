48fd834da3ae0f5d2c47b99fcc4c8bcc09fdd2db
boniseixas <bonisousa@live.com>
2023-08-18 09:28:05 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: boniseixas <bonisousa@live.com>

---
+++ src/list/operacoesBasicas/TestaTarefa.java
@@ -0,0 +1,18 @@
+package list.operacoesBasicas;
+
+public class TestaTarefa {
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricaoTarefas();
+    }
+}
